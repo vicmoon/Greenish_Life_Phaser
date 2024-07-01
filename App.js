c7fc26a7bd66a8572c3df2ee583f3ba3211b71c2
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-07-01 20:35:34 +0200

    update

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Game from "./src/components/Game";
 import "./style.css";
 import "./main";
 
 function App() {
   return (
     <div className="App">
-      <div id="phaser-container"></div>
+      <div id="canvas"></div>
     </div>
   );
 }
